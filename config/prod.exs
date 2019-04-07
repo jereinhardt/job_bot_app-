7665ae325d134f6a1a9de44d5e07176df74068cd
Josh Reinhardt <joshua.e.reinhardt@gmail.com>
2019-04-07 14:37:23 -0500

    add distillery and set configs for first release

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,16 @@ use Mix.Config
 config :job_bot, JobBotWeb.Endpoint,
   load_from_system_env: true,
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true,
+  secret_key_base: "${SECRET_KEY_BASE}"
+
+config :job_bot, JobBot.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: "${DATABASE_URL}",
+  database: "",
+  ssl: true,
+  pool_size: 1
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -61,4 +70,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
