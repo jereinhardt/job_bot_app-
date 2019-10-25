78f8879332b0ba4e3283c020a39b603fd87b0104
Josh Reinhardt <joshua.e.reinhardt@gmail.com>
2019-10-24 19:38:44 -0500

    update simply hired crawler

--- lib/job_bot/crawlers/simply_hired.ex
+++ lib/job_bot/crawlers/simply_hired.ex
@@ -32,6 +32,7 @@ defmodule JobBot.Crawler.SimplyHired do
           |> Floki.parse()
           |> extract_listing_data_from_parsed_body()
           |> Map.put(:listing_url, url)
+          |> Map.put(:application_url, url)
 
         {:ok, listing}        
       {:error, %Error{reason: reason}} ->
@@ -46,14 +47,13 @@ defmodule JobBot.Crawler.SimplyHired do
   defp extract_urls_from_index({:ok, %Response{status_code: 200, body: body}}) do
     body
     |> Floki.parse()
-    |> Floki.attribute("a.card-link.js-job-link", "href")
+    |> Floki.attribute(".jobposting-title a.card-link", "href")
     |> Enum.map(&relative_to_absolute_url(@base_url, &1))
   end
   defp extract_urls_from_index(_), do: []
 
   defp extract_listing_data_from_parsed_body(parsed) do
     %Listing{
-      application_url: extract_application_url(parsed),
       city: extract_city(parsed),
       company_name: extract_company_name(parsed),
       description: extract_description(parsed),
@@ -62,23 +62,15 @@ defmodule JobBot.Crawler.SimplyHired do
     }
   end
 
-  defp extract_application_url(parsed) do
-    url =
-      parsed
-      |> Floki.attribute(".viewjob-controls .apply a", "href")
-      |> Enum.at(0)
-    relative_to_absolute_url(@base_url, url)
-  end
-
   defp extract_city(parsed) do
     parsed
-    |> Floki.find(".job-info .location")
+    |> Floki.find(".viewjob-header .location")
     |> Floki.text()  
   end
 
   defp extract_company_name(parsed) do
     parsed
-    |> Floki.find(".job-info .company")
+    |> Floki.find(".viewjob-header .company")
     |> Floki.text()
   end
 
@@ -90,7 +82,7 @@ defmodule JobBot.Crawler.SimplyHired do
 
   defp extract_title(parsed) do
     parsed
-    |> Floki.find("h1.viewjob-header-title")
+    |> Floki.find(".viewjob-header h1")
     |> Floki.text()
   end
 end
